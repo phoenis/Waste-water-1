b52c14f71acef30d89d53e8994f980d2f36467ae
phoenis <mara.cominardi@gmail.com>
2017-01-14 21:15:35 +0100

    START

--- sketch.js
+++ sketch.js
@@ -4,47 +4,91 @@ var buttonStart, buttonBath, buttonKitchen;
 var stateStart=false;
 var stateBath=false;
 var stateKitchen=false;
+var Jack;
+var name=input.value();
 
 function preload() {
     myBg = loadImage('images/background.png');
+    Jack = loadImage('images/Jack_Start.png');
 }
 
 function setup() {
     createCanvas(windowWidth,windowHeight);
     
-    // Start to Bathroom
-    buttonStart = createButton('start');
-    buttonStart.position(width/3,height/2);
+/////////////////////////////////////////// BUTTONS
+// Start to Bathroom
+    buttonStart = createButton("→<br>Let's go!");
+    buttonStart.addClass('buttons');
+    buttonStart.size(250,90);
+    buttonStart.position(width/2,height/3*2);
     buttonStart.mousePressed(StartToBath);
-    buttonStart.hide();
-    // Bathroom to Kitchen
+// Bathroom to Kitchen
     buttonBath = createButton('bath');
-    buttonBath.position(width/2,height/2);
+    buttonBath.position(width/2,height/3*2);
     buttonBath.mousePressed(BathToKitchen);
-    buttonBath.hide();
-    // Kitchen to Laundry
+//    buttonBath.hide();
+// Kitchen to Laundry
     buttonKitchen = createButton('kitchen');
-    buttonKitchen.position(width/3*2,height/2);
+    buttonKitchen.position(width/3*2,height/3*2);
     buttonKitchen.mousePressed(KitchenToLaundry);
-    buttonKitchen.hide();
-    
+//    buttonKitchen.hide();
+
+/////////////////////////////////////////// START
+// INPUT NAME
+    input = createInput();
+    input.id("inputName");
+    input.position(width/2,height/2);
+    input.size(250,40);
+    // Placeholder
+    var input = document.getElementById ("inputName");
+    input.placeholder = "your name..";
 }
 
-function draw() {  
-    // Background
+function draw(){  
+/////////////////////////////////////////// BACKGROUND
     image(myBg,x,y,width*3,height*2);
     
-    // Start to Bathroom
-    buttonStart.show();
+/////////////////////////////////////////// JACK <3
+    push();
+    imageMode(CENTER);
+    image(Jack,width/5,height/3*2,height/2.4,height/1.5);
+    pop();
+    
+/////////////////////////////////////////// START
+// TEXT - Question
+    push();
+    fill(255);
+    textSize(width/20);
+    textAlign(CENTER);
+    textStyle(BOLD);
+    textFont("Dosis");
+    text('HOW MUCH WATER DO YOU USE?',width/2,y+height/6);
+    pop();
+// TEXT - Insert name
+    push();
+    fill(255);
+    textSize(40);
+    textAlign(RIGHT);
+    textFont("Arial");
+    text('Hi,',width/2-20,y+height/2+33);
+    pop();
+    
+// BUTTON - Start to Bathroom
     if(stateStart==true){
         y=y-10;
 
         if (y<-height) {
             y=-height;
-        }
-        buttonStart.hide();
+        };
+        buttonStart.hide();  
+/*||||||||||||||||||||||||||||||||| CHIEDERE ||||||||||||||||||||||||||||||||*/
+        document.getElementById("inputName").style.display='none';
+    
     }
-    // Bathroom to Kitchen
+
+
+/////////////////////////////////////////// BATHROOM
+// BUTTON - Bathroom to Kitchen
     if(stateBath==true){
         x=x-10;
 
@@ -52,7 +96,9 @@ function draw() {
             x=-width;
         }
     }
-    // Kitchen to Laundry
+
+/////////////////////////////////////////// KITCHEN
+// BUTTON - Kitchen to Laundry
     if(stateKitchen==true){
         x=x-10;
 
@@ -60,22 +106,32 @@ function draw() {
             x=-width*2;
         }
     }
+    
+/////////////////////////////////////////// LAUNDRY
+    
+    
 }
 
-// Start to Bathroom
+/////////////////////////////////////////// TRANSLATE BACKGROUND
+    // Start to Bathroom
 function StartToBath() {
     if(stateStart==false){
         stateStart=true;
-    } 
+    }  
+    
+    var abc = document.getElementById("inputName").value;
+    document.getElementById("demo").innerHTML = abc;
+    
+    text(abc,30,30);
 }
-// Bathroom to Kitchen
+    // Bathroom to Kitchen
 function BathToKitchen() {
     if(stateBath==false){
         stateStart=false;
         stateBath=true;
     } 
 }
-// Kitchen to Laundry
+    // Kitchen to Laundry
 function KitchenToLaundry() {
     if(stateKitchen==false){
         stateBath=false;
